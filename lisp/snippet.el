fb7d581b3a91319406570f860d9d2faec99e6708
Petteri Suckman <petteri.suckman@relexsolutions.com>
2015-09-29 14:07:36 +0300

    Added file save functionality to the snippet saver.

--- lisp/snippet.el
+++ lisp/snippet.el
@@ -1,15 +1,18 @@
-;;; Work in progress...
+;;;; Saves snippets to a buffer, with comments.
+;;;; Author: Petteri Suckman
 (defun save-snippet-to-buffer ()
   "Save highlighted part of the text to another buffer with comments"
   (interactive)
   (progn
     (kill-ring-save (point-min) (point-max) t) ;; highlighted text to clipboard
     (switch-to-buffer-other-window "*SNIPPETS*")
+    (goto-char (point-max))
     (insert (format-time-string "Snippet added: %Y-%m-%d  %H:%M:%S\n"))
     (insert "****BEGIN_SNIPPET****\n")
     (yank)
     (insert "\n****END_SNIPPET****\n")
-    (switch-to-buffer (other-buffer -1))
+    (write-file (concat "~/emacs/snippets/snippet_" (format-time-string "%Y-%m-%d") ".txt"))
+    (previous-buffer)
     (message "Moved the snippet to buffer *SNIPPETS*")
     ))
 
