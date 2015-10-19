6f6d143a362e1c9c9919caec40ec0f7ac9528b41
Petteri Suckman <petteri.suckman@relexsolutions.com>
2015-10-19 14:59:12 +0300

    Made Hundred style according to JavaRanch style guide.

--- cattledrive/java-1/Hundred.java
+++ cattledrive/java-1/Hundred.java
@@ -2,28 +2,35 @@
  * how to use variables, how to concatenate strings, how to use a loop effectively,
  * and how to output to the screen (standard out).
  *
+ * Author: Petteri S.
  */
 class Hundred {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         final int MAX_ROW_LENGTH = 80;
         final int MAX_PRINT = 100;
-
         int printCounter = 0;
-        int rowCounter = 0;
+        int rowCharacterCounter = 0;
         String inputString = "";
-        if (args.length > 0) {
+
+        if (args.length > 0)
+        {
             inputString = args[0];
         }
 
-        while (printCounter < MAX_PRINT) {
+        while (printCounter < MAX_PRINT)
+        {
 
-            if (rowCounter < MAX_ROW_LENGTH - inputString.length()) {
+            if (rowCharacterCounter < MAX_ROW_LENGTH - inputString.length())
+            {
                 System.out.print(inputString + " ");
-                rowCounter = rowCounter + inputString.length() + 1;
-                printCounter ++;
-            } else {
+                rowCharacterCounter = rowCharacterCounter + inputString.length() + 1;
+                printCounter++;
+            }
+            else
+            {
                 System.out.println("");
-                rowCounter = 0;
+                rowCharacterCounter = 0;
             }
         }
     }
