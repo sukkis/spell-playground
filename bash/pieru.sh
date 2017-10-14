092fb376beef26fd2ae9aaeabb9859d37cba6770
Petteri Suckman <petteri.suckman@relexsolutions.com>
2017-10-14 19:25:29 +0300

    Fart sounds on command line:)

---
+++ bash/pieru.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for i in {1..5}; do afplay fart-01.wav; afplay fart-02.wav; done
---
+++ bash/pieru2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for pieru in fart-01.wav fart-02.wav  fart-03.wav fart-04.wav fart-05.wav fart-06.wav fart-07.wav fart-08.wav fart-squeak-01.wav fart-squeak-02.wav fart-squeak-03.wav; do afplay fart-01.wav; afplay $pieru; done
