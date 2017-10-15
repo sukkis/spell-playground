148df5cba604d5c450adc00ca01c61398a1b25d1
Petteri Suckman <petteri.suckman@relexsolutions.com>
2017-10-15 15:02:36 +0300

    Removed own directory structure form pieru2

--- bash/pieru2.sh
+++ bash/pieru2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-D='/Users/petterisuckman/Downloads/'
+D='/Users/user/Downloads/'
 
 for pieru in fart-01.wav fart-02.wav  fart-03.wav fart-04.wav fart-05.wav fart-06.wav fart-07.wav fart-08.wav fart-squeak-01.wav fart-squeak-02.wav fart-squeak-03.wav; do afplay $D$pieru; done
