8a62b0c6dad42251600e56cb288ea0d789e4cd6b
Petteri Suckman <petsa@sukkeli.com>
2017-11-27 21:27:17 +0200

    First  save for Xmonad configurations.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,28 +3,34 @@ import XMonad.Layout.NoBorders
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Actions.WorkspaceNames
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Cursor
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Graphics.X11.ExtraTypes.XF86                -- allows referring to extra keys with convenience names
 
-main = do
-    xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig {
+main = xmonad . myConfig =<< spawnPipe "xmobar"
+
+myConfig pipe = defaultConfig {
+        startupHook = startup,
         manageHook = myManageHook,
         layoutHook = smartBorders . avoidStruts $ layoutHook defaultConfig, -- smartBorders removes borders from full screen app
         handleEventHook = docksEventHook <+> handleEventHook defaultConfig,
-        logHook = dynamicLogWithPP xmobarPP
-                       {
-                       ppOutput = hPutStrLn xmproc,
-                       ppTitle = xmobarColor "Green" "" . shorten 50
-                       },
+        logHook = myLogHook pipe,
         terminal = "urxvt",
         workspaces = myWorkSpaces,
         modMask = mod4Mask -- Mod to Windows key
     } `additionalKeys` myShortCuts
 
 -- Workspace names
-myWorkSpaces = ["1-urxvt","2-code","3-firefox","4-urxvt","5-urxvt","6-urxvt","7-messaging","8-keepassx","9-minimize"]
+myWorkSpaces = ["1-urxvt","2-code","3-firefox","4-urxvt","5-viewer","6-spotify","7-messaging","8-keepassx","9-minimize"]
+
+-- Make workspace names visible in Xmobar
+myLogHook pipe = workspaceNamesPP xmobarPP {
+  ppOutput = hPutStrLn pipe,
+  ppTitle  = xmobarColor "Red" "" . shorten 50}
+                 >>= dynamicLogWithPP 
 
 
 -- Define ManageHooks. With composeAll you can combine options, like in "= manageDocks <+> manageHook defaultConfig"
@@ -39,9 +45,18 @@ myManageHook = composeAll [
 -- Define keyboard shortcuts
 myShortCuts = [
     ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock"),
-    ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"),   -- print screen with resizable picture area
-    ((0, xK_Print), spawn "scrot"),                           -- regular print screen
+    ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"),                     -- print screen with resizable picture area
+    ((0, xK_Print), spawn "scrot"),                                             -- regular print screen
     ((mod4Mask .|. shiftMask, xK_e), spawn "emacs"),
     ((mod4Mask .|. shiftMask, xK_f), spawn "firefox"),
-    ((mod4Mask .|. shiftMask, xK_t), sendMessage ToggleStruts) -- toggle Xmobar visible/hidden
+    ((mod4Mask .|. shiftMask, xK_t), sendMessage ToggleStruts),                 -- toggle Xmobar visible/hidden
+    ((0                     , xF86XK_AudioLowerVolume), spawn "amixer -q sset Master 2%-"),  -- lower volume
+    ((0                     , xF86XK_AudioRaiseVolume), spawn "amixer -q sset Master 2%+"),  -- raise volume
+    ((0                     , xF86XK_AudioMute), spawn "amixer set Master toggle")    -- mute
     ]
+
+-- On starting Xmonad
+startup :: X ()
+startup = do
+spawn "feh --bg-fill ~/Pictures/wallpapers/miami.jpg &"
+setDefaultCursor xC_pirate
