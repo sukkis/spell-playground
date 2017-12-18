19e9aa39bb34b46c5998021b7e2b0ca63b197f4c
Petteri Suckman <petsa@sukkeli.com>
2017-12-18 06:42:46 +0200

    Non-working draft of Back to the future exercise.

---
+++ dev_academy/backToTheFuture.js
@@ -0,0 +1,79 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+/*
+
+
+    January & October
+    April & July
+    September & December
+    February & March & November
+    
+    The input is a string of the date he arrived in the past: 
+    "Monday 1 January"
+If there is a matching day/date you will need to return the following string: 
+"I'm leaving here on Monday 1 October
+If there is no matching day/date that year, 
+Marty is stuck in the past and you will need 
+to return the following string: 
+"Doc, I can't get back to the future!" 
+
+*/
+function solution(input) {
+  inputArray = input.split(' ', 3);
+  weekday    = inputArray[0];
+  dayOfMonth = inputArray[1];
+  month      = inputArray[2];
+  
+  monthBack  = "";
+  
+  switch (month) {
+    case 'January':
+      monthBack = "October"; //31
+      break;
+ 
+    case 'February':
+      monthBack = 'March';  //31
+      break;
+    
+    case 'March':  //31
+      monthBack = 'November'; //30   -----
+      break;
+    
+    case 'April':
+      monthBack = 'July'; //31
+      break;
+
+    case 'September':
+      monthBack = 'December'; //31
+      break;
+    
+    default :
+      monthBack = "NO";
+      break
+  }
+    
+  if ((monthBack !== "NO") && !((month === 'March') && (dayOfMonth == 31))) {
+     return "I'm leaving here on " + weekday + " " + dayOfMonth + " of " + monthBack;
+  } else {
+     return "Doc, I can't get back to the future!"; 
+  }
+  
+}
+
+function isValidDay(day, month) {
+    if (day == 31 && month === 'March') {
+      return false;
+    } else {
+      return true;
+    }
+}
+console.log("weekday : " + weekday);
+console.log("dayOfMonth : " + dayOfMonth);
+console.log("month : " + month);
+console.log(solution('Monday 1 September'));
