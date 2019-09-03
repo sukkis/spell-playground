44eb87ced22ba0488ff0379d4fe7493d5e8a8425
Petteri Suckman <petsa@sukkeli.com>
2019-09-03 21:18:16 +0300

    Another version

---
+++ haskell/poker2.hs
@@ -0,0 +1,36 @@
+{-# OPTIONS_GHC -Wall #-}
+
+import System.Random
+
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
+  | Jack | Queen | King | Ace
+  deriving (Eq, Ord, Enum, Show, Read)
+
+data Suit = Clubs | Diamonds | Hearts | Spades
+  deriving (Eq, Ord, Enum, Show, Read)
+
+data Card = Card Rank Suit
+  deriving (Eq, Show, Read)
+
+type Hand = [Card]
+
+randomRank :: IO Int
+randomRank = randomRIO (0,12)
+
+randomSuit :: IO Int 
+randomSuit = randomRIO (0,3) 
+
+fullDeck :: [Card]
+fullDeck = [ Card j i | i <- [Clubs .. Spades], j <- [Two .. Ace] ]
+
+getCard :: Int -> Int -> Card
+getCard x y = Card ([Two .. Ace] !! x) ([Clubs .. Spades] !! y)
+
+randomCard :: IO ()
+randomCard = randomRank >>= \rank -> randomSuit >>= \suit -> print (getCard rank suit)
+
+runTwice :: Monad m => m b -> m b
+runTwice f = do 
+  _ <- f
+  f 
+
