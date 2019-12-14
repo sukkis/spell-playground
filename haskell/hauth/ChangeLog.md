9a36ce8f6234dd45b7d6e71137d10ba9bd9c19f0
Petteri Suckman <petsa@sukkeli.com>
2019-12-14 19:20:28 +0200

    hauth - package.yaml to include classy-prelude

---
+++ haskell/hauth/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for hauth
+
+## Unreleased changes
---
+++ haskell/hauth/src/Lib.hs
@@ -0,0 +1,12 @@
+module Lib
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+
+add :: Int -> Int
+add s  = 1 + s 
+
+
