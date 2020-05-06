aa846909e1ff029056cd1b0a7546427265e1d434
Petteri Suckman <petteri.suckman@relexsolutions.com>
2020-05-06 23:57:31 +0300

    Removed unnecessary stuff.

---
+++ haskell/bintree2.hs
@@ -0,0 +1,26 @@
+data Tree = Leaf
+    | Node Int Tree Tree
+    deriving Show
+
+-- Gets value of a node.
+getValue :: Tree -> Int
+getValue Leaf         = 0
+getValue (Node x _ _) = x
+
+-- Inserts a value into a Tree, in order.
+insertTreeValue :: Int -> Tree -> Tree
+insertTreeValue x (Leaf) = Node x Leaf Leaf
+insertTreeValue x (Node y left right)
+  | getValue (Node y left right) > x  = Node x (Node y left right) Leaf
+  | (getValue left) <= x              = Node y left (insertTreeValue x right)
+  | (getValue left) > x               = Node y (insertTreeValue x left) right
+
+-- Convert a tree into a list
+treeToList :: Tree -> [Int]
+treeToList (Leaf)              = []
+treeToList (Node x Leaf Leaf)  = [x]
+treeToList (Node x left right)  =
+  let l =  getValue (Node x left right) : treeToList left
+      r =  treeToList right
+  in
+      l ++ r
