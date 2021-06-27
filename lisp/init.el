b435f0f42ccac1dfa95f1b70a30fd9b1e605cc3a
Petteri Suckman <petteri.suckman@gmail.com>
2021-06-27 10:40:20 +0300

    Lightweight Emacs config from personal computer.

---
+++ lisp/init.el
@@ -0,0 +1,29 @@
+;; Initialize package sources
+(require 'package)
+
+
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Enable robot-mode for Robot Framework file editing
+(load-file "/home/petsa/.emacs.d/extend/robot-mode/robot-mode.el")
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . robot-mode))
+
+;; Modify looks & startup
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+;(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)          ; Disable the menu bar
+(load-theme 'tango-dark t)  ; Other good theme: 'misterioso' 
+
+
+;; Keyboard shortcuts
+(global-set-key (kbd "<f5>")  'toggle-truncate-lines) ;; changes between wrapping lines and truncating $
