cfbc1ce3b27193f11cd86dbdccbba9532e2d218a
Petteri Suckman <petteri.suckman@relexsolutions.com>
2021-08-28 17:20:49 +0300

    Emacs python package listing improved.

--- elisp/init.el
+++ elisp/init.el
@@ -1,14 +1,31 @@
 ;; Initialize package sources
 (require 'package)
 
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
+
 (unless package-archive-contents
   (package-refresh-contents))
+
+;; myPackages contains a list of package names
+(defvar myPackages
+  '(better-defaults                 ;; Set up some better Emacs defaults
+    blacken
+    pylint
+    flycheck
+    material-theme                  ;; Theme
+    )
+  )
+
+;; Scans the list in myPackages
+;; If the package listed is not already installed, install it
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      myPackages)
 
 ;; Delete whitespace from files.
 (add-hook 'write-file-hooks 'delete-trailing-whitespace)
@@ -21,12 +38,6 @@
 (require 'nix-mode)
 (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
 
-;; Need a linter for python. Pylint?
-
-;; Jedi Python auto-completion package -- this is buggy
-;;(add-hook 'python-mode-hook 'jedi:setup)
-;;(setq jedi:complete-on-dot t)                 ; optional
-
 ;; Modify looks & startup
 (setq inhibit-startup-message t)
 (set-frame-font 'fira-code-emacs' nil t)
@@ -34,28 +45,16 @@
 (tool-bar-mode -1)          ; Disable the toolbar
 ;(tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
-(menu-bar-mode -1)          ; Disable the menu bar
-(load-theme 'tango-dark t)  ; Other good theme: 'misterioso'
-
+;(menu-bar-mode -1)          ; Disable the menu bar
+(load-theme 'material t)  ; Other good theme: 'misterioso', 'tango-dark'
+;;(global-linum-mode t)       ; Show line numbers everywhere
 
 ;; Keyboard shortcuts
 (global-set-key (kbd "<f5>")  'toggle-truncate-lines) ;; changes between wrapping lines and truncating $
 (global-set-key (kbd "C-z") 'shell)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;;
- ;; Took away Jedi 2021-08-25 due to it being buggy.
- '(package-selected-packages (quote (flycheck virtualenv nix-mode)))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 ;; Starts Emacs so that you can just "emacsclient myfile"
 ;;   to open buffers in the existing session
