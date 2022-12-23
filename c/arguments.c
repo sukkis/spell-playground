13f0acbe483d53d5fcb8b38473a7282b24a625b8
Petteri Suckman <petteri.suckman@gmail.com>
2022-12-23 23:36:02 +0200

    Fixed poor formatting.

--- c/arguments.c
+++ c/arguments.c
@@ -6,31 +6,32 @@
 
 char* clean_name(char *str)
 {
-       	int n = 3;
+     int n = 3;
 
-	if (strncmp(str, "./", n)){
-		str = &str[2];
-	}
+     if (strncmp(str, "./", n)){
+	  str = &str[2];
+     }
 
-	return str;
+     return str;
 }
 
 int main(int argc, char *argv[])
 {
+     printf("Number of arguments passed: %4d \n", argc);
 
-	printf("Number of arguments passed: %4d \n", argc);
-	if (argc >= 1) {
-		printf("Name of program and %d other arguments. \n", argc -1);
-	} else {
-		printf("Something went wrong in calling the program.");
-	}
+     if (argc >= 1) {
+	  printf("Name of program and %d other arguments. \n", argc -1);
+     } else {
+	  printf("Something went wrong in calling the program.");
+     }
 
-	printf("Name of the program is: %s \n",clean_name(argv[0]));
+     printf("Name of the program is: %s \n",clean_name(argv[0]));
 
-	int i;
+     int i;
 
-	for (i = 1; i < argc; i++){
-		printf("Argument %d is %s \n", i, argv[i]);
-	}
-	return 0;
+     for (i = 1; i < argc; i++){
+	  printf("Argument %d is %s \n", i, argv[i]);
+     }
+
+     return 0;
 }
