87cbad8292624a0f751c31766371c5c58ed6e2cc
Petteri Suckman <petteri.suckman@relexsolutions.com>
2023-04-13 14:15:02 +0300

    Make special characters explicit in output.

---
+++ c/1-10_make_specials_visible.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Make special characters tab \t, backspace \b and backslash \\ visible in
+ * output. */
+
+void main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        } else if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+        } else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        } else {
+            putchar(c);
+        }
+    }
+}
